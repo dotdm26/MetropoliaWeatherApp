3ba904e52c6bf17ce5c2620d60868a1c5091690a
andreilegit <andreile@metropolia.fi>
2020-03-11 13:18:54 +0200

    Merge branch 'master' of C:\Users\tonyt\AndroidStudioProjects\MetropoliaWeatherApp with conflicts.

--- app/src/main/java/com/example/metropoliaweatherapp/MainActivity.java
+++ app/src/main/java/com/example/metropoliaweatherapp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.metropoliaweatherapp;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.os.Bundle;
-import java.util.ArrayList;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import static com.example.metropoliaweatherapp.PreferencesActivity.LOCATION;
+import static com.example.metropoliaweatherapp.PreferencesActivity.SHARED_PREFS;
 
 public class MainActivity extends AppCompatActivity {
     /**
@@ -18,20 +39,47 @@ public class MainActivity extends AppCompatActivity {
      * Calendar button to review past dates' information
      */
 
+    private Button calendarButton;
+    private Button userButton;
+    private Button listButton;
+    private Elements source;
+    private TextView parse;
+    private TextView mTextViewResult;
+    private TextView temps;
+    private RequestQueue mQueue;
+    EditText cityName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        Button userButton = findViewById(R.id.userButton);
-        userButton.setOnClickListener(new View.OnClickListener() {
+        temps = findViewById(R.id.temperature);
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                prefNew();
+            public void onClick(View v) {
+                jsonParse();
+                jsonParse2();
             }
         });
 
-        Button listButton = findViewById(R.id.listButton);
+        setContentView(R.layout.activity_main);
+
+        userButton = findViewById(R.id.userButton);
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                prefAdd();
+            }
+        });
+
+        listButton = findViewById(R.id.listButton);
         listButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,33 +87,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button calendarButton = findViewById(R.id.calendarButton);
+        calendarButton = findViewById(R.id.calendarButton);
         calendarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 calendar();
             }
         });
-
-        Spinner prefSpinner = findViewById(R.id.savedprefsSpinner);
-        ArrayAdapter adapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_spinner_item,
-                (ArrayList) GlobalModel.getInstance().getPreferences());
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        prefSpinner.setAdapter(adapter);
-        prefSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
-        });
     }
 
-    public void prefNew() {
+    public void clearWeather() {
+        mTextViewResult = findViewById(R.id.text_view_result);
+        temps = findViewById(R.id.temperature);
+
+        String clear = "";
+
+        mTextViewResult.setText(clear);
+        temps.setText(clear);
+    }
+
+    public void prefAdd() {
         Intent pref = new Intent(this, PreferencesActivity.class);
         startActivity(pref);
     }
@@ -80,4 +121,66 @@ public class MainActivity extends AppCompatActivity {
         startActivity(list);
     }
 
+    private void jsonParse() {
+        clearWeather();
+        String location = GlobalModel.getInstance().getPreferences().get(1).getLocation();
+        String cName = location;
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cName + "&units=metric&appid=b9572d546f224251f9983505002bbe7c";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("weather");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject weather = jsonArray.getJSONObject(i);
+
+                                String main = weather.getString("main");
+                                String description = weather.getString("description");
+
+                                mTextViewResult.append(main + ", " + description);
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+
+    private void jsonParse2() {
+        clearWeather();
+        String location = GlobalModel.getInstance().getPreferences().get(1).getLocation();
+        String cName = location;
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + cName + "&units=metric&appid=b9572d546f224251f9983505002bbe7c";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject("main");
+                            for (int i = 0; i < jsonObject.length(); i++) {
+
+                                int temp = jsonObject.getInt("temp");
+
+                                temps.setText(String.valueOf(temp));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
 }
